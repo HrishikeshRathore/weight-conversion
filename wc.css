188098abf7a4ea95b09b2866baac6007b3b49964
Tanya45 <50803970+Tanya45@users.noreply.github.com>
2019-06-03 15:20:22 +0530

    Create wc.css

---
+++ wc.css
@@ -0,0 +1,101 @@
+   
+
+body
+  {
+      background:linear-gradient(90deg, #200122, #6f0000,#200122);      /*linear gradient in the background of webpage. i.e., background making stylish*/
+  }
+
+
+h2 
+  {  
+      font-size: 50px;
+      background-color: rgba(150,150,150,.3);
+      color: #fff;                                       /*making the heading stylish which appears at the top of the web page*/
+      text-shadow: 5px 5px 5px black;
+      font-family: 'Cabin Sketch', cursive;
+      text-align: center;
+  }
+
+
+label
+  {
+      color: white;
+      text-shadow: 1px 1px 1px #f0f1f2;           /*making the labels stylish which appears at the input top*/
+  }
+
+
+input
+  {
+      border-radius: 10px;              /* the input box where user input the value he need to convert*/
+      background-color: #f1f2f3;
+  }
+
+
+p.abcd
+  {
+      text-align:center;
+      font-size: 20px;             /*decorating the output user gets*/
+      color: white;
+  }
+
+
+div.center
+  {
+      text-align: center;         /*division that align the text to the center*/
+  }
+
+
+.row
+  {
+      display: flex;          /*making the rows comfortable*/
+      width: 100%;
+  }
+
+
+.row::after
+  {                   
+      clear: both;            /* next row get the properties after each row*/
+      content: "";
+  }
+
+
+.col-1{width:8.33%;}
+.col-2{width:16.66%;}
+.col-3{width:25%;}
+.col-4{width:33.33%;}
+.col-5{width:41.66%;}
+.col-6{width:50%;}              /*columns that are the part of rows*/
+.col-7{width:58.33%;}
+.col-8{width:66.66%;}
+.col-9{width:75%;}
+.col-10{width:83.33%;}
+.col-11{width:91.66%;}
+.col-12{width:100%;}
+
+*
+  {
+      box-sizing: border-box;     /*used for comfort of columns*/
+  }
+
+
+[class*="col-"]
+  {
+      float: left;            /*class of columns is designed*/
+      padding: 15px;
+  }
+
+
+footer
+  {
+      font-size: 12px;
+      position: absolute;         /*makes the footer rext stylish and proper aligned*/
+      bottom:5px;
+      text-align: center;
+      width: 100%;
+      background-color: #200122;
+      color: white;
+  }
+
+  
+ 
+  
